8b161bbfe7a9f6214724ededcc996064be73446e
Evelyn Oliveira <evelyn@MacBook-Air-de-Evelyn.local>
2025-08-20 13:55:30 -0300

    Ajuntou app em INSTALLED_APPS

--- app/views.py
+++ app/views.py
@@ -1,4 +1,4 @@
 from django.shortcuts import render
 
 def classificacao(request):
-    return render(request, "app/index.html")  # inclui a subpasta do app
+    return render(request, "app/index.html")
